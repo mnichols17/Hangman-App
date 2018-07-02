c47077bbd089b6d6d0f2c3894e7cedab127ffffb
Mike Nichols <mike.nichols97@gmail.com>
2018-07-02 15:40:46 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 let correct = 0;
 let wrongCount = 0;
+let wins = 0;
 
+// Creates a new game
 document.getElementById('newGame_button').onclick = function(){
   document.getElementById("newGame").setAttribute('hidden',true);
+  document.getElementById('guessString').style.color = "#18EA7E";
   $('.btn-light').prop('disabled',false);
   var buttons = document.getElementsByClassName('btn-light');
   for(button of buttons){button.style.background = "white";}
@@ -11,6 +14,7 @@ document.getElementById('newGame_button').onclick = function(){
   getWord();
 };
 
+// Gets the word that the user will guess
 function getWord(){
   let xhr = new XMLHttpRequest();
   const url = "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=ab0d5274c4e036127a40d059f3c06ea27056d56b0e0300d75"
@@ -33,6 +37,7 @@ function getWord(){
   xhr.send();
 }
 
+// The game being played
 function playGame(word){
   document.getElementById('panel_1').innerHTML = `<h1>${wrongCount}</h1>`;
   let wordArray = word.toUpperCase().split("");
@@ -54,12 +59,13 @@ function playGame(word){
         wrongCount++;
       }
       document.getElementById('panel_1').innerHTML = `<h1>${wrongCount}</h1>`;
-      if(correct == wordArray.length){endGame(true);}
-      else if(wrongCount >= 7) {endGame(false);}
+      if(correct == wordArray.length){endGame(true, word);}
+      else if(wrongCount >= 7) {endGame(false, word);}
     }
   }
 }
 
+// Checks if the word is valid and playable
 function checkWord(word){
   if(word.length > 12){return false;}
   let badChars = [" ","-",",",".","/"];
@@ -69,6 +75,7 @@ function checkWord(word){
   return true;
 }
 
+// Checks if the guessed letter is in the word
 function checkGuess(guess, wordArray, guessArray){
   for(let i in wordArray){
     if(wordArray[i] == guess){
@@ -79,18 +86,23 @@ function checkGuess(guess, wordArray, guessArray){
   setGuess(guessArray);
 }
 
+// Places correctly guessed letters into the correct spaces
 function setGuess(guessArray){
   let output = "";
   for(let char of guessArray){ output += char + " ";}
-  document.getElementById('panel_2').innerHTML = `<h2>${output}</h2>`;
+  document.getElementById('guessString').innerHTML = `${output}`;
 }
 
-function endGame(result){
+// Called when the game is over. 'New Game' button is now visible
+function endGame(result, word){
   $('.btn-light').prop('disabled',true);
   document.getElementById("newGame").removeAttribute('hidden');
   if(result){
     document.getElementById('panel_1').innerHTML = `<h1>You won!</h1>`;
+    wins++;
+    document.getElementById('winCount').innerHTML = `Win Count: ${wins}`;
   } else {
     document.getElementById('panel_1').innerHTML = `<h1>You lost!</h1>`;
+    document.getElementById('guessString').innerHTML = `${word.toUpperCase()}`;
   }
 }
